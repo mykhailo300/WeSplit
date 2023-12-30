be5a729942f80c4f8547dd3c4953fbce4bb93520
Mykhailo <dmytrivmykhailo2@gmail.com>
2023-12-30 22:07:23 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Михайло Дмитрів on 11.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
